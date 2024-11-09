115c2e8e4959d3cc3fa75af82bd4c962ffe5d7bc
Kushal <kushalsharmahacker678@gmail.com>
2024-11-09 07:54:30 +0530

    Second Commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
 // client/src/App.js
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Components
 import TaskList from './components/TaskList/TaskList';
+import Home from './pages/HomePage'
+import Login from './components/Auth/Login';
+
 
 const App = () => {
   return (
-    <div>
-      <TaskList />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/task" element={<TaskList />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 };
 
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='bg-slate-900 min-h-screen'>
+      
+    </div>
+  )
+}
+
+export default HomePage
